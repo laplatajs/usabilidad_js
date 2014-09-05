6aed55fb1fd27e5addf4d81434fd6561d7d03d14
Diego Paez <paez.di@gmail.com>
2014-09-04 21:07:59 -0300

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  voltaire = require('bespoke-theme-voltaire'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash'),
+  state = require('bespoke-state'),
+  forms = require('bespoke-forms');
+
+// Bespoke.js
+bespoke.from('article', [
+  voltaire(),
+  keys(),
+  touch(),
+  bullets('li, .bullet'),
+  scale(),
+  //hash(),
+  state(),
+  forms()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+
